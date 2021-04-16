9473237d664f5a7c5df3a2d5dd1b89da116b7df1
WLBF <lbf1353@live.com>
2021-04-16 14:28:51 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/links-japan/log
+
+go 1.15
+
+require (
+	github.com/certifi/gocertifi v0.0.0-20200922220541-2c3bb06c6054 // indirect
+	github.com/evalphobia/logrus_sentry v0.8.2
+	github.com/getsentry/raven-go v0.2.0 // indirect
+	github.com/getsentry/sentry-go v0.10.0
+	github.com/sirupsen/logrus v1.8.1
+)
---
+++ log.go
@@ -0,0 +1,88 @@
+package log
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/evalphobia/logrus_sentry"
+	"github.com/getsentry/sentry-go"
+	"github.com/sirupsen/logrus"
+	"runtime"
+)
+
+// Create a new instance of the logger. You can have any number of instances.
+var log = logrus.New()
+
+func Init(dsn string, prod bool) {
+
+	if len(dsn) == 0 || !prod {
+		return
+	}
+
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:   dsn,
+		Debug: true,
+	})
+	if err != nil {
+		return
+	}
+
+	hook, err := logrus_sentry.NewSentryHook(dsn, []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+	})
+	hook.StacktraceConfiguration.Skip = 2
+	hook.StacktraceConfiguration.Enable = true
+	if err == nil {
+		log.Hooks.Add(hook)
+	}
+}
+
+func Info(msg string) {
+	log.Info(msg)
+}
+
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+
+func Infoln(args ...interface{}) {
+	log.Infoln(args...)
+}
+
+func JSON(v interface{}) {
+	msg, _ := json.MarshalIndent(v, "", "    ")
+	log.Info(string(msg))
+}
+
+func Warn(msg string) {
+	log.WithFields(logrus.Fields{
+		"func": trace(1),
+	}).Warn(msg)
+}
+
+func Warnf(format string, args ...interface{}) {
+	log.WithFields(logrus.Fields{
+		"func": trace(1),
+	}).Warnf(format, args...)
+}
+
+func Error(err error) {
+	log.WithFields(logrus.Fields{
+		"func": trace(1),
+	}).Error(err)
+}
+
+func Error2(err error) {
+	log.WithFields(logrus.Fields{
+		"func": trace(2),
+	}).Error(err)
+}
+
+func trace(n int) string {
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[n]) // get the 2nd one, the caller
+	file, line := f.FileLine(pc[n])
+	return fmt.Sprintf("%s:%d %s\n", file, line, f.Name())
+}
