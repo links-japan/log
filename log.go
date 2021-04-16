b6f83c430394631b645a4f70f3a31e7dfce6a49f
WLBF <lbf1353@live.com>
2021-04-16 16:40:57 +0800

    update

--- log.go
+++ log.go
@@ -1,13 +1,10 @@
 package log
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/evalphobia/logrus_sentry"
 	"github.com/getsentry/sentry-go"
 	"github.com/sirupsen/logrus"
 	"os"
-	"runtime"
 )
 
 // Create a new instance of the logger. You can have any number of instances.
@@ -40,63 +37,30 @@ func Init() {
 	}
 }
 
-func Info(args ...interface{}) {
-	log.Info(args...)
-}
-
-func Fatal(args ...interface{}) {
-	log.Fatal(args...)
-}
-
-func WithField(key string, value interface{}) *logrus.Entry {
-	return log.WithField(key, value)
-}
-
-func Printf(format string, args ...interface{}) {
-	log.Printf(format, args...)
-}
-
-func Infof(format string, args ...interface{}) {
-	log.Infof(format, args...)
-}
-
-func Infoln(args ...interface{}) {
-	log.Infoln(args...)
-}
-
-func JSON(v interface{}) {
-	msg, _ := json.MarshalIndent(v, "", "    ")
-	log.Info(string(msg))
-}
-
-func Warn(msg string) {
-	log.WithFields(logrus.Fields{
-		"func": trace(1),
-	}).Warn(msg)
-}
-
-func Warnf(format string, args ...interface{}) {
-	log.WithFields(logrus.Fields{
-		"func": trace(1),
-	}).Warnf(format, args...)
-}
-
-func Error(err error) {
-	log.WithFields(logrus.Fields{
-		"func": trace(1),
-	}).Error(err)
-}
-
-func Error2(err error) {
-	log.WithFields(logrus.Fields{
-		"func": trace(2),
-	}).Error(err)
-}
-
-func trace(n int) string {
-	pc := make([]uintptr, 10) // at least 1 entry needed
-	runtime.Callers(2, pc)
-	f := runtime.FuncForPC(pc[n]) // get the 2nd one, the caller
-	file, line := f.FileLine(pc[n])
-	return fmt.Sprintf("%s:%d %s\n", file, line, f.Name())
-}
+func WithField(key string, value interface{}) *logrus.Entry { return log.WithField(key, value) }
+func WithFields(fields logrus.Fields) *logrus.Entry         { return log.WithFields(fields) }
+func WithError(err error) *logrus.Entry                     { return log.WithError(err) }
+func Debugf(format string, args ...interface{})             { log.Debugf(format, args...) }
+func Infof(format string, args ...interface{})              { log.Infof(format, args...) }
+func Printf(format string, args ...interface{})             { log.Printf(format, args...) }
+func Warnf(format string, args ...interface{})              { log.Warnf(format, args...) }
+func Warningf(format string, args ...interface{})           { log.Warningf(format, args...) }
+func Errorf(format string, args ...interface{})             { log.Errorf(format, args...) }
+func Fatalf(format string, args ...interface{})             { log.Fatalf(format, args...) }
+func Panicf(format string, args ...interface{})             { log.Panicf(format, args...) }
+func Debug(args ...interface{})                             { log.Debug(args...) }
+func Info(args ...interface{})                              { log.Info(args...) }
+func Print(args ...interface{})                             { log.Print(args...) }
+func Warn(args ...interface{})                              { log.Warn(args...) }
+func Warning(args ...interface{})                           { log.Warning(args...) }
+func Error(args ...interface{})                             { log.Error(args...) }
+func Fatal(args ...interface{})                             { log.Fatal(args...) }
+func Panic(args ...interface{})                             { log.Panic(args...) }
+func Debugln(args ...interface{})                           { log.Debugln(args...) }
+func Infoln(args ...interface{})                            { log.Infoln(args...) }
+func Println(args ...interface{})                           { log.Println(args...) }
+func Warnln(args ...interface{})                            { log.Warnln(args...) }
+func Warningln(args ...interface{})                         { log.Warningln(args...) }
+func Errorln(args ...interface{})                           { log.Errorln(args...) }
+func Fatalln(args ...interface{})                           { log.Fatalln(args...) }
+func Panicln(args ...interface{})                           { log.Panicln(args...) }
